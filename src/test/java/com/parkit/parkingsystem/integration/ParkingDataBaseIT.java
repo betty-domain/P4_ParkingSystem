971de5b522e630adc505e6f279ac15f9c3ca402d
betty Domain <betty.domain@free.fr>
2020-12-07 17:35:40 +0100

    Implémentation des tests d'intégration entre service Parking et Database

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,10 +1,16 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
+import com.parkit.parkingsystem.util.ClockUtil;
+import com.parkit.parkingsystem.util.DateConvertUtil;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,18 +19,32 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
 
+
     private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+    private static DateConvertUtil dateConvertUtil;
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
 
+    private final String vehicleRegistrationNumber = "ABCDEF";
+
+    private ParkingService parkingService;
+
     @Mock
-    private static InputReaderUtil inputReaderUtil;
+    private static InputReaderUtil inputReaderUtilMock;
+
+    @Mock
+    private static ClockUtil clockUtilMock;
 
     @BeforeAll
     private static void setUp() throws Exception{
@@ -32,14 +52,17 @@ public class ParkingDataBaseIT {
         parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
         ticketDAO.dataBaseConfig = dataBaseTestConfig;
+        dateConvertUtil = new DateConvertUtil();
         dataBasePrepareService = new DataBasePrepareService();
     }
 
     @BeforeEach
     private void setUpPerTest() throws Exception {
-        when(inputReaderUtil.readSelection()).thenReturn(1);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        when(inputReaderUtilMock.readSelection()).thenReturn(1);
+        when(inputReaderUtilMock.readVehicleRegistrationNumber()).thenReturn(vehicleRegistrationNumber);
         dataBasePrepareService.clearDataBaseEntries();
+
+        parkingService = new ParkingService(inputReaderUtilMock, parkingSpotDAO, ticketDAO, clockUtilMock);
     }
 
     @AfterAll
@@ -49,17 +72,46 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        when(clockUtilMock.getCurrentDate()).thenReturn(new Date());
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+
+        //TODO : voir deux asserts dans la même méthode
+
+        //check that a ticket is actually saved in DB
+        Ticket registeredTicket = ticketDAO.getTicket(vehicleRegistrationNumber);
+
+        assertNotNull(registeredTicket);
+
+        int availableSpot = parkingSpotDAO.getNextAvailableSlot(registeredTicket.getParkingSpot().getParkingType());
+        //check Parking table is updated with availability
+        assertNotEquals(availableSpot, registeredTicket.getParkingSpot().getId());
+
     }
 
     @Test
     public void testParkingLotExit(){
-        testParkingACar();
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        LocalDateTime entryDateTime = LocalDateTime.of(2020,1,1,10,10,10);
+        int hoursOfParking = 3;
+        LocalDateTime exitDateTime = entryDateTime.plusHours(hoursOfParking);
+
+        int initialAvailableSpot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
+        when(clockUtilMock.getCurrentDate()).thenReturn(dateConvertUtil.convertToDate(entryDateTime));
+        parkingService.processIncomingVehicle();
+
+        when(clockUtilMock.getCurrentDate()).thenReturn(dateConvertUtil.convertToDate(exitDateTime));
+        parkingService = new ParkingService(inputReaderUtilMock, parkingSpotDAO, ticketDAO, clockUtilMock);
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+
+        Ticket registeredTicket = ticketDAO.getTicket(vehicleRegistrationNumber);
+        int availableSpot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
+        //TODO : à revoir car plusieurs asserts sur cette méthode
+        assertEquals(initialAvailableSpot, availableSpot);
+        assertEquals(Fare.CAR_RATE_PER_HOUR * hoursOfParking,registeredTicket.getPrice());
+        assertEquals(DateConvertUtil.convertToDate(exitDateTime), registeredTicket.getOutTime());
     }
 
 }
